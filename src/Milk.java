bf6d56669c70fbfe8f7a5074bf4644cd76f9fa8d
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-03-05 23:21:55 +0500

    added class Milk

---
+++ src/Milk.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class Milk extends DairyProduct{
+
+    /**жирность*/
+    private byte fatness;
+    private final static Random r = new Random();
+    private static int titleNumber;
+
+    static {
+        Milk.titleNumber = 0;
+    }
+
+    /**
+     * Параметризированный конструктор:
+     * @param title    название
+     * @param price    уена
+     * @param quantity количество
+     * @param unit     единица измерения
+     * @param fat      процент жирности
+     */
+    public Milk(String title, double price, short quantity, String unit, byte fat) {
+        super(title, price, quantity, unit);
+        this.type = "liquid";
+        this.fatness = fat;
+    }
+
+    /**
+     * Конструктор, создающий продукт по умолчанию.
+     */
+    public Milk() {
+        this(String.format("Dairy#%d", ++Milk.titleNumber),
+                r.nextDouble(), (short) r.nextInt(50),
+                "kg",(byte) r.nextInt(20));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s Fatness: %d%s",
+                super.toString(),
+                this.fatness,
+                "%;");
+    }
+}
