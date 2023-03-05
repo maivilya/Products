0fd8a88a7e865f9b4d6b97e6ce7222d4690dc76a
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-03-05 23:40:16 +0500

    improve class Milk

--- src/Milk.java
+++ src/Milk.java
@@ -30,12 +30,12 @@ public class Milk extends DairyProduct{
     public Milk() {
         this(String.format("Milk#%d", ++Milk.titleNumber),
                 r.nextDouble(), (short) r.nextInt(50),
-                "kg",(byte) r.nextInt(20));
+                "liter",(byte) r.nextInt(20));
     }
 
     @Override
     public String toString() {
-        return String.format("%s Fatness: %d%s",
+        return String.format("%s Fatness: %d%s\n",
                 super.toString(),
                 this.fatness,
                 "%;");
