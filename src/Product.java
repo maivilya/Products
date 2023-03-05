7b6f4c57fce87800d3353b087d6a637469eca69d
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-03-05 22:16:30 +0500

    class product

---
+++ src/Product.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+
+public class Product {
+    private String title;
+    private double price;
+    private short quantity;
+    private final String unit;
+    private final static Random r = new Random();
+    private static int titleNumber;
+    static {
+        Product.titleNumber = 0;
+    }
+
+
+    /**
+     * Параметризированный конструктор:
+     * @param title    название
+     * @param price    цена
+     * @param quantity количество
+     * @param unit     единица измерения
+     */
+    public Product(String title, double price, short quantity, String unit) {
+        this.title = title;
+        this.price = price;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+    /**
+     * Конструктор, создающий продукт по умолчанию.
+     */
+    public Product() {
+        this(String.format("Title#%d", ++Product.titleNumber),
+                r.nextDouble(), (short) r.nextInt(50), "kg");
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s:\n Price: %.2f$;\n Quantity: %d;\n Unit: %s;\n",
+                this.getClass().getSimpleName(),
+                this.title,
+                this.price,
+                this.quantity,
+                this.unit);
+    }
+}
