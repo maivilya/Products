61f8db875adb67ceb139fe502e36995ee870c6ef
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-03-06 17:15:52 +0500

    added class drinks

---
+++ src/Drinks.java
@@ -0,0 +1,47 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Drinks extends Product{
+
+    /**объем*/
+    private double volume;
+    private Calendar calendar;
+    private static int titleNumber;
+    static {
+        Drinks.titleNumber = 0;
+    }
+
+    /**
+     * Параметризированный конструктор:
+     * @param title    название
+     * @param price    уена
+     * @param quantity количество
+     * @param unit     единица измерения
+     * @param volume   объем
+     */
+    public Drinks(String title, double price, short quantity, String unit, double volume) {
+        super(title, price, quantity, unit);
+        this.calendar = new GregorianCalendar(2023,3, 25);
+        this.volume = volume;
+    }
+
+    /**
+     * Конструктор, создающий продукт по умолчанию.
+     */
+    public Drinks() {
+        this(String.format("Title#%d", ++Drinks.titleNumber),
+                r.nextDouble(), (short) r.nextInt(50), "liter", 0.3);
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s Volume: %f%s;\n Shelf Live: %s-%s-%s;\n",
+                super.toString(),
+                this.volume,
+                "lit",
+                this.calendar.get(Calendar.YEAR),
+                this.calendar.get(Calendar.MONTH),
+                this.calendar.get(Calendar.DAY_OF_MONTH));
+    }
+}
