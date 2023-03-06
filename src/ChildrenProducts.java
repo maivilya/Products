42abb54fc715651d69633db432c75a2a17987529
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-03-06 18:02:19 +0500

    added class children products

---
+++ src/ChildrenProducts.java
@@ -0,0 +1,42 @@
+public class ChildrenProducts extends Product{
+
+    private byte minAge;
+    protected boolean hyperallergenicity;
+    private static int titleNumber;
+    static {
+        ChildrenProducts.titleNumber = 0;
+    }
+
+    /**
+     * Параметризированный конструктор:
+     * @param title    название
+     * @param price    цена
+     * @param quantity количество
+     * @param unit     единица измерения
+     * @param minAge   минимальный возраст
+     */
+    public ChildrenProducts(String title, double price, short quantity,
+                            String unit, byte minAge) {
+        super(title, price, quantity, unit);
+        this.minAge = minAge;
+        this.hyperallergenicity = false;
+    }
+
+    /**
+     * Конструктор, создающий продукт по умолчанию.
+     */
+    public ChildrenProducts() {
+        this(String.format("Toy#%d", ++ChildrenProducts.titleNumber),
+                r.nextDouble(), (short) r.nextInt(50), "gram",
+                (byte)r.nextInt(5));
+        this.hyperallergenicity = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s Minimal Age to use: %d;\n Hyperallergenicity: %b;\n",
+                super.toString(),
+                this.minAge,
+                this.hyperallergenicity);
+    }
+}
